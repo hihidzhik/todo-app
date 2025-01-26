883c783dd48046fa393f72d91408499dcee3abc1
Veronika Vasilieva <veronika.000vas@gmail.com>
2025-01-27 02:53:01 +0300

    timer and functional

--- src/components/TasksFilter.jsx
+++ src/components/TasksFilter.jsx
@@ -1,28 +1,30 @@
 import PropTypes from 'prop-types';
 
-const TasksFilter = ({ activeFilter, onFilterChange }) => {
+function TasksFilter({ activeFilter, onFilterChange }) {
   const filters = ['All', 'Active', 'Completed'];
 
   return (
     <ul className="filters">
       {filters.map((filter) => (
         <li key={filter}>
-          <button className={filter === activeFilter ? 'selected' : ''} onClick={() => onFilterChange(filter)}>
+          <button
+            type="button"
+            className={filter === activeFilter ? 'selected' : ''}
+            onClick={() => onFilterChange(filter)}
+          >
             {filter}
           </button>
         </li>
       ))}
     </ul>
   );
-};
+}
 
 TasksFilter.propTypes = {
   activeFilter: PropTypes.string.isRequired,
   onFilterChange: PropTypes.func.isRequired,
 };
 
-TasksFilter.defaultProps = {
-  activeFilter: 'All',
-};
+TasksFilter.defaultProps = {};
 
 export default TasksFilter;
